7d3eb2983e7b33ceeec86dc3ed0c803d88f3d1ba
Cleisson <cleissonconstc@gmail.com>
2021-05-28 20:09:55 -0300

    :tada: second commit changes

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ server.set('view engine', 'ejs')
 //habilitar arquivos estáticos(arquivos imutáveis(imagens,videos, css, js))
 server.use(express.static("public"));
 
+//usar o request.body
+server.use(express.urlencoded({ extended: true }))
+
 //routes | está usando as rotas, que foi requerido de outro arquivo para melhorar organização
 server.use(routes)
 
